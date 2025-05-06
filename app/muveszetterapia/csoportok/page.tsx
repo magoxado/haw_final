ac98a719d611bd6d00be090733fc43ae543d5b4f
magoxado <magocs.lorant@gmail.com>
2025-05-06 23:56:51 +0200

    Update page.tsx

--- app/muveszetterapia/csoportok/page.tsx
+++ app/muveszetterapia/csoportok/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link"
 export default function PrivacyPolicyPage() {
   return (
     <div className="container mx-auto px-4 py-12">
-      <h1>Csoportok</h1>
 
       <div className="max-w-3xl mx-auto bg-muted p-8 rounded-lg mb-8">
-        <p className="text-lg mb-6">
-        A csoportok szervezés alatt vannak.
-        Kérjük, nézz vissza később!
+        <p className="text-lg mb-6 flex items-center justify-center h-32 font-bold text-center">
+          A csoportok szervezés alatt vannak.
+          <br />
+          Kérjük, nézz vissza később!
         </p>
       </div>
 
