2d2a99076bf968523040646a6275b20aa8717bf8
magoxado <magocs.lorant@gmail.com>
2025-05-07 05:20:37 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin", "latin-ext"] })
 
@@ -31,6 +32,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
